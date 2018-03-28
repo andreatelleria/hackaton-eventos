424bc73ac3f0e42ac7170950de2d9c25fdfdc435
andreatelleria <andrea.telleria@gmail.com>
2018-03-27 19:56:27 -0500

    Clases de estilo corregidas

--- src/components/header-nav-list.js
+++ src/components/header-nav-list.js
@@ -2,7 +2,7 @@ import React from 'react';
 import HeaderListItem from './header-list-item';
 
 const HeaderNavList = ({ items }) => (
-  <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
+  <ul className="nav navbar-nav ml-auto w-100 justify-content-end">
     {items.map(item =>
       <HeaderListItem
         key={item.name}
