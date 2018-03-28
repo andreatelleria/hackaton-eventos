f432851bcf6d1fe6966484debaf9f20a5315859e
LidiaRamirez <lid_26_90@hotmail.com>
2018-03-28 08:41:09 -0500

    Login

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,6 @@
-import React from 'react'
 import Carousel from './carousel'
 import CardEventList from './CardEventList'
-import Login from './Login'
-import firebase from 'firebase'
+import React from 'react'
 
 const dataEvents = [
   {title: 'Congreso de JavaScripts', name:'Laboratoria', url:'http://vitemprende.es/wp-content/uploads/2016/02/image004.jpg', organizer:'Laboratoria', address:'av. Venezuela 123, Barranco'},
@@ -10,26 +8,15 @@ const dataEvents = [
   {title: 'Congreso de JavaScripts', name:'Laboratoria2', url:'http://vitemprende.es/wp-content/uploads/2016/02/image004.jpg', organizer:'Laboratoria', address:'av. Venezuela 123, Barranco'}
 ]
 
-firebase.initializeApp({
-  apiKey: "AIzaSyByqvButDPrBlzb9AIrlYPh9LOoy5Jnpjk",
-  authDomain: "reto-eventos.firebaseapp.com",
-  databaseURL: "https://reto-eventos.firebaseio.com",
-  projectId: "reto-eventos",
-  storageBucket: "reto-eventos.appspot.com",
-  messagingSenderId: "558847801184"
-})
-
-
-const Main = () => {
-    return (
+const Home = () => {
+  return (
       <div className="container">
         <Carousel />
         <CardEventList 
           itemEvents = {dataEvents}
         />
-        <Login />
-      </div>      
-    );
-  }
+      </div>
+  );
+}
 
-  export default Main ;
+  export default Home ;
---
+++ src/components/loginPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from './login'
+
+const loginPage = () => {
+    return (
+      <div >        
+        <Login />
+      </div>      
+    );
+  
+}
+export default loginPage
