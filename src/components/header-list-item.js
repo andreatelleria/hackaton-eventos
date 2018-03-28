83edec179206052815cfc1929e81f7d8f9029ac9
andreatelleria <andrea.telleria@gmail.com>
2018-03-27 23:20:09 -0500

    Cambiando estilos de links

--- src/components/header-list-item.js
+++ src/components/header-list-item.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HeaderListItem = ({ name, href }) => (
   <li className="nav-item">
-    <a className="nav-link" href="{href}">{name}</a>
+    <a className="nav-link" href="{href}"><span>{name}</span></a>
   </li>
 )
 
